3f2711d51b4fa50418e8f6e63816678bd36a8d26
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2021-12-12 04:52:35 +0700

    Add files via upload

---
+++ tugas6.js
@@ -0,0 +1,9 @@
+for ( var a=0; a<=20; a++) {
+    if (a % 1 == 0) {
+        if (a % 2 == 0) {
+            console.log("ini adalah bilangan genap")
+        }else{
+            console.log(a)
+        }
+    }
+}
